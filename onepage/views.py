5401debc6891ee47c8c615842f362127ee1a0e03
xumara <humaraguliyeva@gmail.com>
2019-03-30 20:07:44 +0400

    Database ready

--- onepage/views.py
+++ onepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import WebsiteCommon, HeaderSection, Menu
 
 # Create your views here.
 def index (request):
-    return render(request, "index.html", {})
+    context = {}
+    context ["common"] = WebsiteCommon.objects.last()
+    context["header"] = HeaderSection.objects.last()
+    context["menu_list"] = Menu.objects.all()
+
+    return render(request, "index.html", context)
 
